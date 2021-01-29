707214c6807b11ca0d19b45027a03dff67d89ac1
Owl-kxy <sincheleivaronald@gmail.com>
2021-01-29 12:24:20 -0500

    Added Auth on users with FiredBase (checked)

--- app/src/main/java/com/ronald/cartrace/LoginActivity.java
+++ app/src/main/java/com/ronald/cartrace/LoginActivity.java
@@ -1,14 +1,88 @@
 package com.ronald.cartrace;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button vbtnLogInUser;
+    EditText vedtpassword, vedtemail;
+
+    // FireBase variable
+    FirebaseAuth vFirebaseAuth;
+
+    // DB variable
+    DatabaseReference vDatabaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        vbtnLogInUser = findViewById(R.id.btnLogInUser);
+        vedtpassword = findViewById(R.id.edtPassword);
+        vedtemail = findViewById(R.id.edtEmail);
+
+        vFirebaseAuth = FirebaseAuth.getInstance();
+        vDatabaseRef = FirebaseDatabase.getInstance().getReference();
+
+        vbtnLogInUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LogIn();
+            }
+        });
     }
+
+    private void LogIn()
+    {
+        String email = vedtemail.getText().toString();
+        String password = vedtpassword.getText().toString();
+
+        if(!email.isEmpty() && !password.isEmpty())
+        {
+            if(password.length()>=6)
+            {
+                vFirebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(LoginActivity.this,"Exito", Toast.LENGTH_SHORT).show();
+                        }
+
+                        else
+                        {
+                            Toast.makeText(LoginActivity.this,"Fail", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+
+            else
+            {
+                Toast.makeText(LoginActivity.this, "Verifique la contraseña",Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        else{
+            Toast.makeText(LoginActivity.this, "Se necesita usuario y password", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 }
--- app/src/main/java/com/ronald/cartrace/SelectOptionAuthActivity.java
+++ app/src/main/java/com/ronald/cartrace/SelectOptionAuthActivity.java
@@ -3,11 +3,15 @@ package com.ronald.cartrace;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectOptionAuthActivity extends AppCompatActivity {
 
     Toolbar vToolbar;
+    Button vbtnRegistrarme, vbtnLogIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +19,22 @@ public class SelectOptionAuthActivity extends AppCompatActivity {
         setContentView(R.layout.activity_select_option_auth);
 
         vToolbar = findViewById(R.id.toolbar);
+        vbtnRegistrarme = findViewById(R.id.btnRegistrarme);
+        vbtnLogIn = findViewById(R.id.btnLogIn);
+
         setSupportActionBar(vToolbar);
         getSupportActionBar().setTitle("Selecciona una opcion");
+
+        // arrow left to back
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        vbtnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intLogIn = new Intent(SelectOptionAuthActivity.this, LoginActivity.class);
+                startActivity(intLogIn);
+            }
+        });
+
     }
 }
