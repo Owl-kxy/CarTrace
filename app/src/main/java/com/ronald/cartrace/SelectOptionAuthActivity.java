1908763f27f7dccc00fb4be0aedc849174251d28
Owl-kxy <sincheleivaronald@gmail.com>
2021-01-28 21:27:11 -0500

    Added toolbar on Auth activity

--- app/src/main/java/com/ronald/cartrace/SelectOptionAuthActivity.java
+++ app/src/main/java/com/ronald/cartrace/SelectOptionAuthActivity.java
@@ -1,14 +1,22 @@
 package com.ronald.cartrace;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class SelectOptionAuthActivity extends AppCompatActivity {
 
+    Toolbar vToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_option_auth);
+
+        vToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(vToolbar);
+        getSupportActionBar().setTitle("Selecciona una opcion");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
